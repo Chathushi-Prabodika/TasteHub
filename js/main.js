de8b076a5726f4384e508b7e7610b195a7e8297a
unknown <chathushiprabodika26@gmail.com>
2026-08-01 18:03:37 +0530

    checking javascript features and remove newsletter validation feature

--- js/main.js
+++ js/main.js
@@ -120,25 +120,3 @@ let currentMode = 'login'; // 'login' or 'register'
         window.location.href = 'index.html';
     }
 
- //newslatter validation  
-const newsletterForm = document.getElementById("newsletterForm");
-const newsletterEmail = document.getElementById("newsletterEmail");
-const emailError = document.getElementById("emailError");
-
-if (newsletterForms) {
-    newsletterForm.addEventListener("submit", function(event) {
-        // Prevent the page from refreshing immediately
-        event.preventDefault(); 
-        
-        const emailValue = newsletterEmail.value.trim();
-
-        // Check if it's empty OR if it doesn't include an '@'
-        if (emailValue === "" || !emailValue.includes("@")) {
-            emailError.style.display = "block"; // Show our custom RED error
-            emailError.textContent = "Please enter a valid email containing '@'.";
-        } else {
-            emailError.style.display = "none"; // Hide error
-            alert("Awesome! You have successfully joined our community.");
-        }
-    });
-}
